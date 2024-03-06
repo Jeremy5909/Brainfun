20bde0ea3be0795d3d4601b5322377b4dd7ba1a3
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2024-03-05 19:33:10 -0700

    add todos, remove add/subtract, optimize some parameters, rename some stuff, add duplicate_to

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ private:
         return result;
     }
 
-    bool isPrime(unsigned long long int num) {
+    bool isPrime(unsigned int num) {
         for (int i = 2; i*i <= num ; i++) {
             if (num % i == 0) {
                 return false;
@@ -23,9 +23,10 @@ private:
         }
         return true;
     }
-
 public:
-    std::string efficient_num(int num) {
+    // TODO add second parameter for things like effecient_num for where to put the temp second num
+    std::string efficient_num(unsigned int num) {
+        // TODO make go both up and down and choose which is best
         if(num < 15) {
             return std::string(num, '+');
         } else {
@@ -44,21 +45,19 @@ public:
             return ">" + std::string(firstClosest, '+') + "[<" + std::string(secondClosest, '+') + ">-]<" + std::string(additional, '+');
         }
     }
-    std::string add(const int& num) {
-        return std::string(num, '+');
-    }
-    std::string subtract(const int& num) {
-        return std::string(num, '-');
-    }
-    std::string mult(const int& currPos, const int& num) {
+
+    // TODO make number that adds or subtractes based on input and effeciently w function
+
+    // TODO make take pos ?
+    std::string mult(const unsigned int& currPos, const unsigned int& num) {
         std::string output;
         output += move(currPos, currPos+1) + ">[<" + std::string(num, '+') + ">-]<";
         return output;
     }
-    std::string set(const int& num = 0) {
+    std::string set(const unsigned int& num = 0) {
         return "[-]" + std::string(num, '+');
     }
-    std::string set_pos(int& currPos, int pos) {
+    std::string set_pos(unsigned int& currPos, unsigned int pos) {
         std::string output;
         if (pos > currPos) {
             output = std::string(pos - currPos, '>');
@@ -70,35 +69,62 @@ public:
         return output;
     }
 
-    std::string move(int currPos, int target) {
+    std::string move(unsigned int currPos, int target) {
         return "[" + set_pos(currPos, target) + "+" + negate_arrows(set_pos(currPos, target)) + "<-]";
     }
 
-    std::string plainText(std::string text) {
+    std::string raw_text(std::string text) {
+        // TODO make way more efficient by getting average and only adding necessary
         std::string output;
         for (auto& character : text) {
             output += efficient_num(int(character)) + "." + set();
         }
         return output;
     }
+
+
+    // Make sure to have room one space after target
+    std::string duplicate_to(unsigned int currPos, unsigned int targetPos) {
+        // TODO make it work even when one space after target isn't clean
+        std::string output;
+        unsigned int startPos = currPos;
+
+        output += "["
+                + set_pos(currPos, targetPos)
+                + "+"
+                + set_pos(currPos, currPos + 1)
+                + "+"
+                + set_pos(currPos, startPos)
+                + "-]"
+                + set_pos(currPos, targetPos + 1)
+                + "["
+                + set_pos(currPos, startPos)
+                + "+"
+                + set_pos(currPos, targetPos + 1)
+                + "-]"
+                + set_pos(currPos, startPos);
+        return output;
+    }
+
+    // TODO Have the type of curr. pos be template of either value or ref
+    // TODO have one to compare value of two positions
 };
 
 int main() {
     Brainfun bf;
 
-    int currPos = 1;
+    unsigned int currPos = 1;
+
+    const int NAME = 1;
 
     std::vector<std::function<std::string()>> instructions = {
-            [&]() { return bf.efficient_num(5); },
-            [&]() { return bf.mult(currPos, 4); },
+            [&]() { return bf.raw_text("Hello!"); },
     };
 
 
     for (const auto& instruction : instructions) {
         std::cout << instruction();
     }
-
-    bf.plainText("hello");
 
     return 0;
 }
