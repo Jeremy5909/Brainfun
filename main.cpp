43f628f41b50ea79633d57ee4f3703e8bff1a629
Jeremy5909 <jeremyherczeg@gmail.com>
2024-03-07 19:41:34 -0700

    update gitignore

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <map>
+#include <string>
+#include <vector>
 #include <iostream>
 
 struct Brainfun {
@@ -25,6 +28,10 @@ private:
         return true;
     }
 public:
+    bool shortMode;
+
+    Brainfun(bool shortMode = true) : shortMode(shortMode) {}
+
     // TODO add second parameter for things like effecient_num for where to put the temp second num
     std::string efficient_num(unsigned int num) {
         // TODO make go both up and down and choose which is best
@@ -78,7 +85,8 @@ public:
         // TODO make way more efficient by getting average and only adding necessary
         std::string output;
         for (auto& character : text) {
-            output += efficient_num(int(character)) + "." + set();
+            output += efficient_num(int(character)) + "." + set(); // Putitin , print , reset
+            if (!shortMode) output += std::string(" //Char ") + character + '\n'; // Comment
         }
         return output;
     }
@@ -89,7 +97,7 @@ public:
         std::string output;
         unsigned int startPos = currPos;
 
-        output += "["
+        output = "["
                 + set_pos(targetPos)
                 + "+"
                 + set_pos(currPos + 1)
@@ -106,20 +114,76 @@ public:
         return output;
     }
 
+    std::string efficient_text(const std::string& text) {
+        std::string output;
+
+        std::vector<std::vector<int>> groups;
+        std::map<char, int> charToGroupIndex;
+
+        //
+        for (char character : text) {
+            bool added = false;
+            for (int i = 0; i < groups.size(); ++i) {
+                auto& group = groups[i];
+                if (abs(group[0] - character) <= 26) {
+                    added = true;
+
+                    group.push_back(static_cast<int>(character));
+                    charToGroupIndex[character] = i;
+                    break; // Stop searching once a suitable group is found
+                }
+            }
+            // If character didn't fit into any group, create a new one
+            if (!added) {
+                groups.push_back(std::vector<int> {static_cast<int>(character)}); // Correctly initialize with the ASCII value
+                charToGroupIndex[character] = groups.size() - 1;
+            }
+        }
+
+        // Set beginning nums
+        for (auto& group : groups) {
+            output += efficient_num(group[0]);
+            output += set_pos(currPos + 1);
+            if (!shortMode) output +=  " //Initialize " + std::to_string(group[0]) + "\n";
+        }
+
+
+        for (auto& character : text) {
+            // Rework groups so they're just one int and not vector
+            int groupPos = charToGroupIndex[character];
+            std::vector<int> currGroup = groups[groupPos];
+
+            output += set_pos(groupPos + 1);
+            int diffBetweenChars = static_cast<int>(character) - currGroup[0];
+
+
+            // Add/Subtract necessary
+            currGroup[0] = currGroup[0] + diffBetweenChars;
+            if (diffBetweenChars > 0) {
+                output += std::string(diffBetweenChars, '+');
+            } else if (diffBetweenChars < 0) {
+                output += std::string(abs(diffBetweenChars), '-');
+            }
+            output += ".";
+        }
+
+        return output;
+    }
+
     // TODO Have the type of curr. pos be template of either value or ref
-    // TODO have one to compare value of two positions
 };
 
 int main() {
-    Brainfun bf;
+    Brainfun bf(false);
 
     std::vector<std::function<std::string()>> instructions = {
-            [&]() { return bf.set_pos(3); },
+            [&]() {return bf.efficient_text("Hello World!");}
     };
 
 
     for (const auto& instruction : instructions) {
         std::cout << instruction();
+        if (!bf.shortMode) std::cout << "\n\n";
     }
 
     return 0;
