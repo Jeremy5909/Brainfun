191602d687382eed857d3d0b02b59ce5b5a223d7
Jeremy5909 <jeremyherczeg@gmail.com>
2024-03-09 19:24:01 -0700

    Naming consistency

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 
 struct Brainfun {
 private:
-    int currPos = 1;
+    int curr_pos = 1;
 
-    std::string negate_arrows(const std::string& arrows) {
-        std::string result = arrows;
-        for(auto& arrow : result) {
+    std::string NegateArrows(const std::string& arrows) {
+        std::string output = arrows;
+        for(auto& arrow : output) {
             if (arrow == '>') {
                 arrow = '<';
             } else if (arrow == '<') {
                 arrow = '>';
             }
         }
-        return result;
+        return output;
     }
 
-    bool isPrime(unsigned int num) {
+    bool IsPrime(unsigned int num) {
         for (int i = 2; i*i <= num ; i++) {
             if (num % i == 0) {
                 return false;
@@ -29,195 +29,194 @@ private:
         return true;
     }
 public:
-    bool shortMode;
+    bool short_mode;
 
-    Brainfun(bool shortMode = true) : shortMode(shortMode) {}
+    Brainfun(bool shortMode = true) : short_mode(shortMode) {}
 
     // TODO add second parameter for things like effecient_num for where to put the temp second num
-    std::string efficient_num(unsigned int num) {
+    std::string EfficientNum(unsigned int num) {
         // TODO make go both up and down and choose which is best
         if(num < 15) {
             return std::string(num, '+');
         } else {
             // Get two closest primes
             int additional = 0;
-            if (isPrime(num)) {
+            if (IsPrime(num)) {
                 additional = 1;
                 num = num - 1;
             }
-            int firstClosest = (int) sqrt(num);
-            while (num % firstClosest != 0) {
-                firstClosest = firstClosest - 1;
+            int first_closest = (int) sqrt(num);
+            while (num % first_closest != 0) {
+                first_closest = first_closest - 1;
             }
-            int secondClosest = (int) num/firstClosest;
+            int second_closest = (int) num / first_closest;
 
-            return ">" + std::string(firstClosest, '+') + "[<" + std::string(secondClosest, '+') + ">-]<" + std::string(additional, '+');
+            return ">" + std::string(first_closest, '+') + "[<" + std::string(second_closest, '+') + ">-]<" + std::string(additional, '+');
         }
     }
 
     // TODO make number that adds or subtractes based on input and effeciently w function
 
-    std::string mult(const unsigned int& num) {
+    std::string Mult(const unsigned int& num) {
         std::string output;
-        output += move(currPos+1) + ">[<" + std::string(num, '+') + ">-]<";
+        output += Move(curr_pos + 1) + ">[<" + std::string(num, '+') + ">-]<";
         return output;
     }
-    std::string mult(unsigned int pos1, unsigned int pos2) {
+    std::string Mult(unsigned int pos1, unsigned int pos2) {
         std::string output;
-        int original_pos = currPos;
+        int original_pos = curr_pos;
 
-        output += set_pos(pos1)
-                + duplicate_to(original_pos+2)
-                + set_pos(pos2)
-                + duplicate_to(original_pos + 3);
+        output += SetPos(pos1)
+                  + DuplicateTo(original_pos + 2)
+                  + SetPos(pos2)
+                  + DuplicateTo(original_pos + 3);
         pos1 = original_pos+2;
         pos2 = original_pos+3;
-        output += set_pos(pos1)
-                + "["
-                + set_pos(pos2)
-                + "["
-                + set_pos(original_pos)
-                + "+"
-                + set_pos(original_pos+1)
-                + "+"
-                + set_pos(pos2)
-                + "-]"
-                + set_pos(original_pos+1)
-                + "["
-                + set_pos(pos2)
-                + "+"
-                + set_pos(original_pos+1)
-                + "-]"
-                + set_pos(pos1)
-                + "-]"
-                + set_pos(pos2)
-                + set()
-                + set_pos(original_pos);
+        output += SetPos(pos1)
+                  + "["
+                  + SetPos(pos2)
+                  + "["
+                  + SetPos(original_pos)
+                  + "+"
+                  + SetPos(original_pos + 1)
+                  + "+"
+                  + SetPos(pos2)
+                  + "-]"
+                  + SetPos(original_pos + 1)
+                  + "["
+                  + SetPos(pos2)
+                  + "+"
+                  + SetPos(original_pos + 1)
+                  + "-]"
+                  + SetPos(pos1)
+                  + "-]"
+                  + SetPos(pos2)
+                  + Set()
+                    + SetPos(original_pos);
         return output;
     }
-
-
-    std::string set(const unsigned int& num = 0) {
+    std::string Set(const unsigned int& num = 0) {
         return "[-]" + std::string(num, '+');
     }
-    std::string set_pos(unsigned int pos) {
+    std::string SetPos(unsigned int pos) {
         std::string output;
-        if (pos > currPos) {
-            output = std::string(pos - currPos, '>');
-        } else if (pos < currPos) {
-            output = std::string(currPos - pos, '<');
+        if (pos > curr_pos) {
+            output = std::string(pos - curr_pos, '>');
+        } else if (pos < curr_pos) {
+            output = std::string(curr_pos - pos, '<');
         }
 
-        currPos = pos;
+        curr_pos = pos;
         return output;
     }
 
-    std::string move(int target) {
-        return "[" + set_pos(target) + "+" + negate_arrows(set_pos(target)) + "<-]";
+    std::string Move(int target) {
+        return "[" + SetPos(target) + "+" + NegateArrows(SetPos(target)) + "<-]";
     }
 
-    std::string raw_text(const std::string& text) {
+    std::string SlowText(const std::string& text) {
         // TODO make way more efficient by getting average and only adding necessary
         std::string output;
         for (auto& character : text) {
-            output += efficient_num(int(character)) + "." + set(); // Putitin , print , reset
-            if (!shortMode) output += std::string(" //Char ") + character + '\n'; // Comment
+            output += EfficientNum(int(character)) + "." + Set(); // Putitin , print , reset
+            if (!short_mode) output += std::string(" //Char ") + character + '\n'; // Comment
         }
         return output;
     }
 
     // Make sure to have room one space after target
-    std::string duplicate_to(unsigned int targetPos) {
+    std::string DuplicateTo(unsigned int targetPos) {
         // TODO make it work even when one space after target isn't clean
         std::string output;
-        unsigned int startPos = currPos;
+        unsigned int original_curr_pos = curr_pos;
 
         output = "["
-                + set_pos(targetPos)
-                + "+"
-                + set_pos(currPos + 1)
-                + "+"
-                + set_pos(startPos)
-                + "-]"
-                + set_pos(targetPos + 1)
-                + "["
-                + set_pos(startPos)
-                + "+"
-                + set_pos(targetPos + 1)
-                + "-]"
-                + set_pos(startPos);
+                 + SetPos(targetPos)
+                 + "+"
+                 + SetPos(curr_pos + 1)
+                 + "+"
+                 + SetPos(original_curr_pos)
+                 + "-]"
+                 + SetPos(targetPos + 1)
+                 + "["
+                 + SetPos(original_curr_pos)
+                 + "+"
+                 + SetPos(targetPos + 1)
+                 + "-]"
+                  + SetPos(original_curr_pos);
         return output;
     }
 
-    std::string efficient_text(const std::string& text) {
+    std::string EfficientText(const std::string& raw_text) {
         std::string output;
 
-        const int CLOSENESS = 20;
-        const int started_pos = currPos;
+        const int ACII_CLOSENESS = 20;
+        const int original_cursor_pos = curr_pos;
 
-        std::vector<int> startNums;
-        std::map<char, int> charToGroupIndex;
+        std::vector<int> group_values;
+        std::map<char, int> char_to_groupPos;
 
-        for (char character : text) {
-            bool added = false;
+        for (char character : raw_text) {
+            bool character_has_group = false;
 
             // Search for suitable startNum
-            for (int i = 0; i < startNums.size(); ++i) {
-                auto& startNum = startNums[i];
-                if (abs(startNum - character) <= CLOSENESS) {
-                    added = true;
+            for (int group_value_index = 0; group_value_index < group_values.size(); ++group_value_index) {
+                auto& group_value = group_values[group_value_index];
+                if (abs(group_value - character) <= ACII_CLOSENESS) {
+                    character_has_group = true;
 
-                    charToGroupIndex[character] = i;
-                    break; // Stop searching once a suitable startNum is found
+                    // Assign character's value to i
+                    char_to_groupPos[character] = group_value_index;
+                    break; // Stop searching once a suitable group_value is found
                 }
             }
             // If character didn't fit into any startNum, create a new one
-            if (!added) {
-                startNums.push_back(character); // Correctly initialize with the ASCII value
-                charToGroupIndex[character] = startNums.size() - 1;
+            if (!character_has_group) {
+                group_values.push_back(character); // Correctly initialize with the ASCII value
+                char_to_groupPos[character] = group_values.size() - 1;
             }
         }
 
         // Initialize beginning numbers
-        for (auto& startNum : startNums) {
-            output += efficient_num(startNum);
-            output += set_pos(currPos + 1);
-            if (!shortMode) output +=  " //Initialize " + std::to_string(startNum) + "\n";
+        for (auto& group_value : group_values) {
+            output += EfficientNum(group_value); // Initialize group value
+            output += SetPos(curr_pos + 1); // Go right
+            if (!short_mode) output += " //Initialize " + std::to_string(group_value) + "\n";
         }
 
         // Printing n stuf
-        for (auto& character : text) {
-            int& startNumPos = charToGroupIndex[character];
-            int& startNum = startNums[startNumPos];
+        for (auto& character : raw_text) {
+            int& group_pos = char_to_groupPos[character];
+            int& group_value = group_values[group_pos];
 
-            output += set_pos(startNumPos + started_pos);
-            int diffBetweenChars = character-startNum;
+            output += SetPos(group_pos + original_cursor_pos);
+            int diffBetweenChars = character - group_value;
 
 
-            // Add/Subtract necessary
-            startNum = startNum + diffBetweenChars;
+            group_value = group_value + diffBetweenChars; // Set group value to right num
+            // Do the printing for what just happened
             if (diffBetweenChars > 0) {
                 output += std::string(diffBetweenChars, '+');
-                if (!shortMode) output += " //Add " + std::to_string(diffBetweenChars) + "\n";
+                if (!short_mode) output += " //Add " + std::to_string(diffBetweenChars) + "\n";
             } else if (diffBetweenChars < 0) {
                 output += std::string(-diffBetweenChars, '-');
-                if (!shortMode) output += " //Subtract " + std::to_string(-diffBetweenChars) + "\n";
+                if (!short_mode) output += " //Subtract " + std::to_string(-diffBetweenChars) + "\n";
             }
             output += ".";
-            if (!shortMode) output += " //Print\n";
+            if (!short_mode) output += " //Print\n";
         }
 
         // Delete initialized nums
-        for (int i = currPos; i > started_pos - 1; --i) {
-            output += set_pos(i); // Go back one
-            output += set(); // Set to 0
+        for (int i = curr_pos; i > original_cursor_pos - 1; --i) {
+            output += SetPos(i); // Go back one
+            output += Set(); // Set to 0
         }
-        if (!shortMode) output += " //Reset initial values to zero\n";
+        if (!short_mode) output += " //Reset initial values to zero\n";
 
         return output;
     }
 
-    static std::string make_sure_bf_neat(std::string text, int wrap = 0) {
+    static std::string CleanupBF(std::string text, int wrap = 0) {
         // Fix going back n forth
         text = std::regex_replace(text, std::regex("<>|><"), "");
 
@@ -238,8 +237,7 @@ int main() {
     Brainfun bf(true);
 
     std::vector<std::function<std::string()>> instructions = {
-            [&](){return bf.set_pos(5);},
-            [&](){return bf.efficient_text("Hello *meow*");},
+            [&](){return bf.EfficientText("Hello *meow*");},
 
     };
 
@@ -247,8 +245,8 @@ int main() {
 
 
     for (const auto& instruction : instructions) {
-        std::cout << Brainfun::make_sure_bf_neat(instruction());
-        if (!bf.shortMode) std::cout << "\n\n";
+        std::cout << Brainfun::CleanupBF(instruction());
+        if (!bf.short_mode) std::cout << "\n\n";
     }
 
     return 0;
